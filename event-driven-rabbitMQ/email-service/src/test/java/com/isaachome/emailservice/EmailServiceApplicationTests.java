e77b49d479a8f297af736c51d7eeb019239d03cd
theisaachome <isaachome.burma@gmail.com>
2026-06-30 22:40:41 +0800

    1st commit

---
+++ event-driven-rabbitMQ/email-service/src/test/java/com/isaachome/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaachome.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
